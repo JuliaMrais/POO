6b4400429a8d2dd25f08f79eca1e3963b6d488b7
Júlia Morais <52293844+JuliaMrais@users.noreply.github.com>
2020-05-06 23:35:41 -0300

    06052020

---
+++ multi1a10.rb
@@ -0,0 +1,16 @@
+puts "Digite um valor:"
+
+a = gets.chomp.to_i
+
+puts "1 * #{ 1 *a}"
+puts "2 * #{ 2 * a}"
+puts "3 * #{ 3 *a}"
+puts "4 * #{ 4 * a}"
+puts "5 * #{ 5 * a}"
+puts "6 * #{ 6 * a}"
+puts "7 * #{ 7 * a}"
+puts "8 * #{ 8 * a}"
+puts "9 * #{ 9 * a}"
+puts "10 * #{ 10 * a}"
+
+gets
