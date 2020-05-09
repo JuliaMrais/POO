fdd6e99f03bc6a7333e709ceea6dd7531a95812d
Júlia Morais <52293844+JuliaMrais@users.noreply.github.com>
2020-05-08 21:18:43 -0300

    08052020

---
+++ Par_impar.rb
@@ -0,0 +1,12 @@
+puts "Dígite um número:"
+
+n = gets.chomp.to_i
+
+resto = n/2
+
+    if (resto = 0 )
+        puts "O número #{n} é par!"
+    else 
+        puts "O número #{n} é ímpar!"
+    end
+gets
